068b963ccb48a45d0746a7fb528905b1e26f070b
Brandon Prieto <xbrandon93x@gmail.com>
2018-01-30 19:37:01 -0500

    Restricción de 5 servidores

--- src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
@@ -6,10 +6,10 @@
 package edu.eci.arsw.blacklistvalidator;
 
 import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
-import edu.eci.arsw.threads.IpMaliciosasThread;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,73 +19,70 @@ import java.util.logging.Logger;
  */
 public class HostBlackListsValidator {
 
-    private static final int BLACK_LIST_ALARM_COUNT=5;
-    
+    private static final int BLACK_LIST_ALARM_COUNT = 5;
+
     /**
-     * Check the given host's IP address in all the available black lists,
-     * and report it as NOT Trustworthy when such IP was reported in at least
-     * BLACK_LIST_ALARM_COUNT lists, or as Trustworthy in any other case.
-     * The search is not exhaustive: When the number of occurrences is equal to
-     * BLACK_LIST_ALARM_COUNT, the search is finished, the host reported as
-     * NOT Trustworthy, and the list of the five blacklists returned.
+     * Check the given host's IP address in all the available black lists, and
+     * report it as NOT Trustworthy when such IP was reported in at least
+     * BLACK_LIST_ALARM_COUNT lists, or as Trustworthy in any other case. The
+     * search is not exhaustive: When the number of occurrences is equal to
+     * BLACK_LIST_ALARM_COUNT, the search is finished, the host reported as NOT
+     * Trustworthy, and the list of the five blacklists returned.
+     *
      * @param ipaddress suspicious host's IP address.
      * @param n
-     * @return  Blacklists numbers where the given host's IP address was found.
+     * @return Blacklists numbers where the given host's IP address was found.
      * @throws java.lang.InterruptedException
      */
-    public List<Integer> checkHost(String ipaddress, int n) throws InterruptedException{
-        
-             
-        LinkedList<Integer> blackListOcurrences=new LinkedList<>();
-        
+    public List<Integer> checkHost(String ipaddress, int n) throws InterruptedException {
+
+        LinkedList<Integer> blackListOcurrences = new LinkedList<>();
+
         ArrayList<IpMaliciosasThread> hilos = new ArrayList<>();
+
+        AtomicInteger ocurrencesCount = new AtomicInteger(0);
         
-        int ocurrencesCount=0;        
-        
-        HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
-        
-        int checkedListsCount=0;              
+        HostBlacklistsDataSourceFacade skds = HostBlacklistsDataSourceFacade.getInstance();
+
+        int checkedListsCount = 0;
         int rango;
-        
-        if(n%2!=0){
-            rango = skds.getRegisteredServersCount()/(n+1);
-        }else{
-            rango=skds.getRegisteredServersCount()/n;
-        }        
-        
-        for(int i=0; i < skds.getRegisteredServersCount() ; i+=rango){
-            IpMaliciosasThread hilo = new IpMaliciosasThread(i, i+rango, ipaddress, blackListOcurrences);            
-            hilos.add(hilo);            
+
+        if (n % 2 != 0) {
+            rango = skds.getRegisteredServersCount() / (n + 1);
+        } else {
+            rango = skds.getRegisteredServersCount() / n;
         }
         
-        for(IpMaliciosasThread h: hilos){
-            h.start();
-        }        
+       
         
-        for(IpMaliciosasThread h: hilos){
+        for (int i = 0; i < skds.getRegisteredServersCount(); i += rango) {
+            IpMaliciosasThread hilo = new IpMaliciosasThread(i, i + rango, ipaddress, blackListOcurrences, ocurrencesCount);
+            hilo.start();
+            hilos.add(hilo);
+        }
+       
+        for (IpMaliciosasThread h : hilos) {
+
             h.join();
-            ocurrencesCount += h.getOcurrencias();
+            ocurrencesCount = h.getOcurrencias();
             checkedListsCount += h.getCheckedListsCount();
             
-        }                                                                              
-        
-        if (ocurrencesCount>=BLACK_LIST_ALARM_COUNT){
-            skds.reportAsNotTrustworthy(ipaddress);
+            
         }
-        else{
+
+        if (ocurrencesCount.get() >= BLACK_LIST_ALARM_COUNT) {
+            skds.reportAsNotTrustworthy(ipaddress);
+        } else {
             skds.reportAsTrustworthy(ipaddress);
-        }                
-        
+        }
+
         System.out.println(ocurrencesCount);
-        
+
         LOG.log(Level.INFO, "Checked Black Lists:{0} of {1}", new Object[]{checkedListsCount, skds.getRegisteredServersCount()});
-        
+
         return blackListOcurrences;
     }
-    
-    
+
     private static final Logger LOG = Logger.getLogger(HostBlackListsValidator.class.getName());
-    
-    
-    
+
 }
--- src/main/java/edu/eci/arsw/blacklistvalidator/IpMaliciosasThread.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/IpMaliciosasThread.java
@@ -3,47 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.eci.arsw.threads;
+package edu.eci.arsw.blacklistvalidator;
 
 import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
 import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
  * @author 2108419
  */
 public class IpMaliciosasThread extends Thread{
-    
-    int ocurrencias = 0;
+
     int a;
     int b;    
     int checkedListsCount = 0;
     String ip;
-    
+    boolean flag = true;
+    int busquedas = 0;
     HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
     LinkedList<Integer> blackListOcurrences;
-    
-    public IpMaliciosasThread(int a, int b, String ip, LinkedList<Integer> blackListOcurrences){
+    AtomicInteger ocurrencias;
+   
+    public IpMaliciosasThread(int a, int b, String ip, LinkedList<Integer> blackListOcurrences, AtomicInteger ocurrencias){
         this.a = a;
         this.b = b;
         this.ip = ip;      
         this.blackListOcurrences = blackListOcurrences;
+        this.ocurrencias = ocurrencias;
     }
-    
+   
     public void run(){
-        for (int i=a;i<b ;i++){            
-            checkedListsCount++;            
-            if (skds.isInBlackListServer(i, ip)){ 
-                System.out.println(i);
-                blackListOcurrences.add(i);                
-                ocurrencias++;                
-            }
-        }               
+        while (flag){
+            for (int i=a;i<b ;i++){            
+                if (skds.isInBlackListServer(i, ip)){
+                    blackListOcurrences.add(i);
+                    ocurrencias.getAndIncrement();
+                }
+                if (ocurrencias.get() == 5){
+                    break;
+                }
+                busquedas++;
+                checkedListsCount++;
+            }          
+            flag = false;   
+        }
     }
-    
-    public int getOcurrencias(){
+
+    public AtomicInteger getOcurrencias() {
         return ocurrencias;
     }
+    
     
     public int getCheckedListsCount(){
         return checkedListsCount;
@@ -52,6 +63,6 @@ public class IpMaliciosasThread extends Thread{
     public LinkedList<Integer> getBlackListOcurrence(){
         return blackListOcurrences;
     }
-    
+
     
 }
